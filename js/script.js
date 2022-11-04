8490a606125395b4a7db1099ac86ab87248fdb3d
DenysPotapov <denys.potapov@ukr.net>
2022-11-04 20:00:28 +0200

    add hero

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  
+    direction: 'horizontal',
+    loop: true,
+    effect: 'coverflow',
+    speed: 1500,
+    grabCursor: true,
+    autoHeight: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    }
+   
+  
+});
+
